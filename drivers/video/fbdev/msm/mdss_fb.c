749cd0d5910e927b51e1ac36859816379b0b357b
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-12-15 07:56:26 +0100

    mdss: use lower case letters for adobe_rgb, dcip3 and srgb to be consistent
    Change-Id: Id7955f4c1675bc4b75a177b2bbbd4117694582b4
    idkwhoiam322: Retain all the nodes for OOS too. This fixes display modes for those that use Lineage as their base.
    Signed-off-by: idkwhoiam322 <snkm1999@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -927,6 +927,9 @@ static ssize_t mdss_fb_set_srgb_mode(struct device *dev,
 static DEVICE_ATTR(SRGB, S_IRUGO | S_IWUSR,
 	mdss_fb_get_srgb_mode, mdss_fb_set_srgb_mode);
 
+static DEVICE_ATTR(srgb, S_IRUGO | S_IWUSR,
+	mdss_fb_get_srgb_mode, mdss_fb_set_srgb_mode);
+
 
 static ssize_t mdss_fb_get_adobe_rgb_mode(struct device *dev,
 		struct device_attribute *attr, char *buf)
@@ -969,6 +972,9 @@ static ssize_t mdss_fb_set_adobe_rgb_mode(struct device *dev,
 static DEVICE_ATTR(Adobe_RGB, S_IRUGO | S_IWUSR,
 	mdss_fb_get_adobe_rgb_mode, mdss_fb_set_adobe_rgb_mode);
 
+static DEVICE_ATTR(adobe_rgb, S_IRUGO | S_IWUSR,
+	mdss_fb_get_adobe_rgb_mode, mdss_fb_set_adobe_rgb_mode);
+
 static ssize_t mdss_fb_get_dci_p3_mode(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
@@ -1008,6 +1014,9 @@ static ssize_t mdss_fb_set_dci_p3_mode(struct device *dev,
 }
 
 static DEVICE_ATTR(DCI_P3, S_IRUGO | S_IWUSR,
+	mdss_fb_get_dci_p3_mode, mdss_fb_set_dci_p3_mode);
+
+static DEVICE_ATTR(dci_p3, S_IRUGO | S_IWUSR,
 	mdss_fb_get_dci_p3_mode, mdss_fb_set_dci_p3_mode);
 
 static ssize_t mdss_fb_get_night_mode(struct device *dev,
@@ -1194,9 +1203,9 @@ static struct attribute *mdss_fb_attrs[] = {
 	&dev_attr_msm_fb_panel_status.attr,
 	&dev_attr_msm_fb_dfps_mode.attr,
 	&dev_attr_measured_fps.attr,
-	&dev_attr_SRGB.attr,
-	&dev_attr_Adobe_RGB.attr,
-	&dev_attr_DCI_P3.attr,
+	&dev_attr_srgb.attr,
+	&dev_attr_adobe_rgb.attr,
+	&dev_attr_dci_p3.attr,
 	&dev_attr_night_mode.attr,
 	&dev_attr_adaption_mode.attr,
 	&dev_attr_oneplus_mode.attr,
